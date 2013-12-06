5e759169650207a547b9865dc034da6c6de32802
dennisjay <djprivat@googlemail.com>
2013-12-06 16:48:44 +0100

    Exercise 4 Nr 4 First try

--- Uebung3/src/CudaExercise3_Sobel.cu
+++ Uebung3/src/CudaExercise3_Sobel.cu
@@ -37,6 +37,10 @@ __device__ int getIndexShared(int tx, int ty) {
 	return ty * BLOCKWIDTH + tx;
 }
 
+__device__ int getIndexSharedBorder(int tx, int ty) {
+	return (ty+1) * BLOCKWIDTH + (tx+1);
+}
+
 //////////////////////////////////////////////////////////////////////////////
 // Host implementation
 //////////////////////////////////////////////////////////////////////////////
@@ -55,6 +59,51 @@ void filterOnHost(float* h_a, float* h_res_cpu) {
 //////////////////////////////////////////////////////////////////////////////
 // Device implementation (kernel)
 //////////////////////////////////////////////////////////////////////////////
+__global__ void filter_Kernel_Border(float* d_a, float* d_res) {
+	// 2D Thread ID
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int i = blockIdx.x * blockDim.x + threadIdx.x; // global thread Ids
+	int j = blockIdx.y * blockDim.y + threadIdx.y; // global thread Ids
+
+	// This part is only for version 2 and 3 of the kernel
+	// declaration of the array in shared memory
+	__shared__ float s_a[(BLOCKWIDTH+2) * (BLOCKHEIGHT+2)];
+	//
+	// filling the array in shared memory
+	// central part
+	s_a[getIndexSharedBorder(tx, ty)] = d_a[getIndexGlobal(i, j)]; 
+	
+	// Idea: Threads at the border load the neighboring part
+	// left part
+	if (tx <= 0) {
+		s_a[getIndexSharedBorder(-1,ty)] = getValueGlobal(d_a, i-1, j); 
+	}
+	// right part
+	if (tx >= BLOCKWIDTH-1) {
+		s_a[getIndexSharedBorder(BLOCKWIDTH,ty)] = getValueGlobal(d_a, i+1, j); 
+	}
+	// downer part
+	if (ty <= 0) {
+		s_a[getIndexSharedBorder(tx,-1)] = getValueGlobal(d_a, i, j-1); 
+	}	
+	// upper part
+	if (ty >= BLOCKHEIGHT-1) {
+		s_a[getIndexSharedBorder(tx,BLOCKHEIGHT)] = getValueGlobal(d_a, i, j+1); 
+	}
+
+	// synchronize
+	__syncthreads() ;
+
+	float Gx = s_a[getIndexSharedBorder(tx-1, ty-1)]+2*s_a[getIndexSharedBorder(tx-1, ty)]+s_a[getIndexSharedBorder(tx-1, ty+1)]
+					-s_a[getIndexSharedBorder(tx+1, ty-1)]-2*s_a[getIndexSharedBorder(tx+1, ty)]-s_a[getIndexSharedBorder(tx+1, ty+1)];
+
+	float Gy = s_a[getIndexSharedBorder(tx-1, ty-1)]+2*s_a[getIndexSharedBorder(tx, ty-1)]+s_a[getIndexSharedBorder(tx+1, ty-1)]
+					-s_a[getIndexSharedBorder(tx-1, ty+1)]-2*s_a[getIndexSharedBorder(tx, ty+1)]-s_a[getIndexSharedBorder(tx+1, ty+1)];
+	
+	d_res[getIndexGlobal(i, j)] = sqrt(Gx * Gx + Gy * Gy);
+}
+
 __global__ void filter_Kernel(float* d_a, float* d_res) {
 	// 2D Thread ID
 	int tx = threadIdx.x;
@@ -137,7 +186,7 @@ __global__ void filter_Kernel(float* d_a, float* d_res) {
 void filterOnDevice(float* d_a, float* d_res) {
 	dim3 dimGrid(GRIDWIDTH, GRIDHEIGHT);
 	dim3 dimBlock(BLOCKWIDTH, BLOCKHEIGHT);
-	filter_Kernel<<< dimGrid, dimBlock >>>( d_a, d_res );
+	filter_Kernel_Border<<< dimGrid, dimBlock >>>( d_a, d_res );
 }
 
 //////////////////////////////////////////////////////////////////////////////
